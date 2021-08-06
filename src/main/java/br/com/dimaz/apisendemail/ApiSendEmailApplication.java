53f98a1b70d7ee6af41ed33cb83d04b22cc217f0
DiogoMS <dev.diogo@gmail.com>
2021-08-06 19:06:25 -0300

    fst

---
+++ src/main/java/br/com/dimaz/apisendemail/ApiSendEmailApplication.java
@@ -0,0 +1,13 @@
+package br.com.dimaz.apisendemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSendEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSendEmailApplication.class, args);
+	}
+
+}
