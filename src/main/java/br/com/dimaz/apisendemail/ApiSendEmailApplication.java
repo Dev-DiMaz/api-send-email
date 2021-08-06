f6d05baaa7474051c497d25320c9e8c8baa4849b
DiogoMS <dev.diogo@gmail.com>
2021-08-06 19:52:48 -0300

    ajustes de estrutura

--- src/main/java/br/com/dimaz/apisendemail/ApiSendEmailApplication.java
+++ src/main/java/br/com/dimaz/apisendemail/ApiSendEmailApplication.java
@@ -2,12 +2,20 @@ package br.com.dimaz.apisendemail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ApiSendEmailApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSendEmailApplication.class, args);
 	}
-
+	@GetMapping
+	@RequestMapping("/")
+	public String startPageApi() {
+		return "{\"api\":\"Api-Send-Email is on!\", \"by\":\"DiMaz\"}";
+	}
 }
