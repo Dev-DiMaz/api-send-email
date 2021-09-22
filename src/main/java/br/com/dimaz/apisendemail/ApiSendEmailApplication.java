a25075b1bc42c182b9cd25cbf863f2084d3c80ef
DiogoMS <dev.diogo@gmail.com>
2021-09-21 23:37:33 -0300

    mudanca application

--- src/main/java/br/com/dimaz/apisendemail/ApiSendEmailApplication.java
+++ src/main/java/br/com/dimaz/apisendemail/ApiSendEmailApplication.java
@@ -2,9 +2,9 @@ package br.com.dimaz.apisendemail;
 
 import java.time.LocalDateTime;
 
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,18 +16,17 @@ public class ApiSendEmailApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSendEmailApplication.class, args);
 	}
+
 	@GetMapping
 	@RequestMapping("/")
-	public String startPageApi() {
+	public String startPageApi() throws Exception {
+
 		JSONObject jObj = new JSONObject();
-		try {
-			jObj.put("api", "Api-Send-Email");
-			jObj.put("status", "On");
-			jObj.put("time", LocalDateTime.now());
-			jObj.put("by", "DiMaz");
-		}catch (Exception e) {
-			// TODO: handle exception
-		}
+		jObj.put("api", "Api-Send-Email");
+		jObj.put("status", "On");
+		jObj.put("time", LocalDateTime.now());
+		jObj.put("by", "DiMaz");
+
 //		return "{\"api\":\"Api-Send-Email is on!\", \"by\":\"DiMaz\"}";
 		return jObj.toString();
 	}
